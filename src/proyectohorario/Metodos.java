b5620ed9545cd90b15d4830b753780bf9005ac89
aalvarezbretana <aalvarezbretana@danielcastelao.org>
2020-03-05 14:29:08 +0100

    falta lectura y depurar

--- src/proyectohorario/Metodos.java
+++ src/proyectohorario/Metodos.java
@@ -7,6 +7,9 @@ package proyectohorario;
 
 import libreriaproyecto.PedirDatos;
 import empleado.Empleado;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
@@ -17,8 +20,14 @@ import java.util.Iterator;
  */
 public class Metodos {
 
+    PrintWriter esc = null;
+    FileWriter arch;
+
     public Empleado crearEmpleado() {
-        Empleado e = new Empleado(PedirDatos.pedirString("Nombre:"), PedirDatos.pedirString("Primer apellido:"), PedirDatos.pedirString("Segundo apellido:"), PedirDatos.pedirInt("Numero horas:"));
+        Empleado e = new Empleado(PedirDatos.pedirNombre("Nombre:(Max.10 caracteres)"),
+                PedirDatos.pedirString("Primer apellido:"),
+                PedirDatos.pedirString("Segundo apellido:"),
+                PedirDatos.pedirHoras("Número de horas:"));
         return e;
     }
 
@@ -42,48 +51,209 @@ public class Metodos {
         return acumulador;
     }
 
-    public void crearHorario(String[][] horario, ArrayList<Empleado> plantilla, String[] dias, String[] horas) {
+    public void crearHorario(String[][] horario, ArrayList<Empleado> plantilla,
+            String[] dias, String[] horas, ArrayList<Integer> horasMax) {
         int aleatorio = (int) Math.floor(Math.random() * (plantilla.size()));
-        int numero = 0;
+        int numero;
         int num_ale;
         int horasT = horasTotales(plantilla);
+        horasMaximas(plantilla, horasMax);
         if (horasT >= 119) {
             for (int k = 0; k < dias.length; k++) {
                 do {
                     num_ale = aleatorio;
-                    aleatorio = (int) Math.floor(Math.random() * (plantilla.size()));
-                    numero = 0;
-                } while (num_ale == aleatorio);
+                    aleatorio = (int) Math.floor(Math.random()
+                            * (plantilla.size()));
+                } while (num_ale == aleatorio || horasMax.get(aleatorio)
+                        == plantilla.get(aleatorio).getNum_horas());
                 numero = 0;
                 for (int i = 0; i < horas.length; i++) {
-                    if (numero == 8) {
+                    if (numero == 8 || horasMax.get(aleatorio)
+                            == plantilla.get(aleatorio).getNum_horas()) {
                         do {
                             num_ale = aleatorio;
-                            aleatorio = (int) Math.floor(Math.random() * (plantilla.size()));
+                            aleatorio = (int) Math.floor(Math.random()
+                                    * (plantilla.size()));
                             numero = 0;
-                        } while (num_ale == aleatorio);
+                        } while (num_ale == aleatorio || horasMax.get(aleatorio)
+                                == plantilla.get(aleatorio).getNum_horas());
                     }
                     horario[i][k] = plantilla.get(aleatorio).getNombre();
+                    horasMax.set(aleatorio, horasMax.get(aleatorio) + 1);
                     numero++;
                 }
             }
             amosarHorario(horario, horas, dias);
-
         } else {
-            System.out.println("No se cumplen las minimas horas para cubrir el horario total semanal");
+            System.err.println("No se cumplen las horas mínimas "
+                    + "para cubrir el horario total semanal(119 horas)");
         }
     }
 
     public void amosarHorario(String[][] horario, String[] horas, String[] dias) {
-        System.out.print("Horas/Días");
-        for (int i = 0; i < dias.length; i++) {
-            System.out.print("   " + dias[i]);
-        }
-        for (int f = 0; f < horario.length; f++) {
-            System.out.print("\n " + horas[f]);
-            for (int c = 0; c < horario[f].length; c++) {
-                System.out.print("       " + horario[f][c] + "  ");
+        if (horario[0][0] == null) {
+            System.out.println("El horario no ha sido creado.");
+        } else {
+            System.out.print("Horas/Días   ");
+            for (int i = 0; i < dias.length; i++) {
+                switch (dias[i].length()) {
+                    case 1:
+                        System.out.print(dias[i] + "          ");
+                        break;
+                    case 2:
+                        System.out.print(dias[i] + "         ");
+                        break;
+                    case 3:
+                        System.out.print(dias[i] + "        ");
+                        break;
+                    case 4:
+                        System.out.print(dias[i] + "       ");
+                        break;
+                    case 5:
+                        System.out.print(dias[i] + "      ");
+                        break;
+                    case 6:
+                        System.out.print(dias[i] + "     ");
+                        break;
+                    case 7:
+                        System.out.print(dias[i] + "    ");
+                        break;
+                    case 8:
+                        System.out.print(dias[i] + "  ");
+                        break;
+                    case 9:
+                        System.out.print(dias[i] + "  ");
+                        break;
+                }
+            }
+            for (int f = 0; f < horario.length; f++) {
+                System.out.print("\n " + horas[f] + "       ");
+                for (int c = 0; c < horario[f].length; c++) {
+                    switch (horario[f][c].length()) {
+                        case 1:
+                            System.out.print(horario[f][c] + "          ");
+                            break;
+                        case 2:
+                            System.out.print(horario[f][c] + "         ");
+                            break;
+                        case 3:
+                            System.out.print(horario[f][c] + "        ");
+                            break;
+                        case 4:
+                            System.out.print(horario[f][c] + "       ");
+                            break;
+                        case 5:
+                            System.out.print(horario[f][c] + "      ");
+                            break;
+                        case 6:
+                            System.out.print(horario[f][c] + "     ");
+                            break;
+                        case 7:
+                            System.out.print(horario[f][c] + "    ");
+                            break;
+                        case 8:
+                            System.out.print(horario[f][c] + "   ");
+                            break;
+                        case 9:
+                            System.out.print(horario[f][c] + "  ");
+                            break;
+                        case 10:
+                            System.out.print(horario[f][c]);
+                            break;
+                    }
+                }
             }
         }
+    }
+
+    public void horasMaximas(ArrayList<Empleado> plantilla, ArrayList<Integer> horasMax) {
+        horasMax.removeAll(horasMax);
+        for (int i = 0; i < plantilla.size(); i++) {
+            horasMax.add(0);
+        }
+    }
+
+    public void imprimir(String ruta, String[][] horario, String[] horas, String[] dias) {
+        try {
+            arch = new FileWriter(ruta);
+            esc = new PrintWriter(arch);
+            esc.print("Horas/Días   ");
+            for (int i = 0; i < dias.length; i++) {
+                switch (dias[i].length()) {
+                    case 1:
+                        esc.print(dias[i] + "          ");
+                        break;
+                    case 2:
+                        esc.print(dias[i] + "         ");
+                        break;
+                    case 3:
+                        esc.print(dias[i] + "        ");
+                        break;
+                    case 4:
+                        esc.print(dias[i] + "       ");
+                        break;
+                    case 5:
+                        esc.print(dias[i] + "      ");
+                        break;
+                    case 6:
+                        esc.print(dias[i] + "     ");
+                        break;
+                    case 7:
+                        esc.print(dias[i] + "    ");
+                        break;
+                    case 8:
+                        esc.print(dias[i] + "  ");
+                        break;
+                    case 9:
+                        esc.print(dias[i] + "  ");
+                        break;
+                }
+            }
+            for (int f = 0; f < horario.length; f++) {
+                esc.print("\n " + horas[f] + "       ");
+                for (int c = 0; c < horario[f].length; c++) {
+                    switch (horario[f][c].length()) {
+                        case 1:
+                            esc.print(horario[f][c] + "          ");
+                            break;
+                        case 2:
+                            esc.print(horario[f][c] + "         ");
+                            break;
+                        case 3:
+                            esc.print(horario[f][c] + "        ");
+                            break;
+                        case 4:
+                            esc.print(horario[f][c] + "       ");
+                            break;
+                        case 5:
+                            esc.print(horario[f][c] + "      ");
+                            break;
+                        case 6:
+                            esc.print(horario[f][c] + "     ");
+                            break;
+                        case 7:
+                            esc.print(horario[f][c] + "    ");
+                            break;
+                        case 8:
+                            esc.print(horario[f][c] + "   ");
+                            break;
+                        case 9:
+                            esc.print(horario[f][c] + "  ");
+                            break;
+                        case 10:
+                            esc.print(horario[f][c]);
+                            break;
+                    }
+                }
+            }
+        } catch (IOException ex) {
+            System.err.println("Error al escribir el archivo" + ex.getMessage());
+        } finally {
+            esc.close();
+        }
+    }
+
+    public void leerEmpleados() {
+
     }
 }
--- src/proyectohorario/ProyectoHorario.java
+++ src/proyectohorario/ProyectoHorario.java
@@ -23,17 +23,21 @@ public class ProyectoHorario {
         Metodos obx = new Metodos();
         ArrayList<Empleado> plantilla = new ArrayList();
         String[][] horario = new String[17][7];
-        String[] horas = {"09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "24:00", "01:00"};
-        String[] dias = {"Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"};
+        String[] horas = {"09:00", "10:00", "11:00", "12:00", "13:00", "14:00",
+            "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", 
+            "22:00", "23:00", "24:00", "01:00"};
+        String[] dias = {"Lunes", "Martes", "Miércoles", "Jueves", "Viernes", 
+            "Sábado", "Domingo"};
+        ArrayList<Integer> horasMax = new ArrayList();
         int resposta;
         do {
-            System.out.println("\nQue queres facer?"
+            System.out.println("\nQué quieres hacer?"
                     + "\n 1.-Añadir empleado."
                     + "\n 2.-Mostrar empleados."
                     + "\n 3.-Crear horario."
                     + "\n 4.-Mostrar horario."
-                    //    + "\n 5.-Ordenar."
-                    + "\n 0.-Saír.");
+                    + "\n 5.-Imprimir horario."
+                    + "\n 0.-Salir.");
             Scanner sc = new Scanner(System.in);
             resposta = sc.nextInt();
             switch (resposta) {
@@ -41,26 +45,22 @@ public class ProyectoHorario {
                     plantilla.add(obx.crearEmpleado());
                     break;
                 case 2:
-                    try{
-                    obx.mostrar(plantilla);
-                    }catch(NullPointerException ex){
-                        System.err.println("Error. "+ex.getMessage());
+                    try {
+                        obx.mostrar(plantilla);
+                    } catch (NullPointerException ex) {
+                        System.err.println("Error. " + ex.getMessage());
                     }
-                    finally{
                     break;
-                    }
                 case 3:
-                    obx.crearHorario(horario, plantilla, dias, horas);
+                    obx.crearHorario(horario, plantilla, dias, horas, horasMax);
                     break;
                 case 4:
                     obx.amosarHorario(horario, horas, dias);
                     break;
-//                 case 5:
-//                    obx.consultarLibro(plantilla);
-//                    break;
+                case 5:
+                    obx.imprimir("horario.txt", horario, horas, dias);
+                    break;
             }
         } while (resposta != 0);
-
     }
-
 }
